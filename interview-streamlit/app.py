c9d93ce2ef6b199dcab6db69d17c802665d944d5
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2023-06-20 02:05:34 +0900

    Updated documentation for the genQuestion function

--- interview-streamlit/app.py
+++ interview-streamlit/app.py
@@ -17,11 +17,21 @@ examples = [
     "I think things out well. When I speak, I speak with conviction. If I feel like it's something that best suits me and my person, I deal with it. I say it. I have no problem speaking out publicly about issues. But for personal things, and for things about personal selfishness, or wanting more money, I don't do that. Once I give my word, that's it. I don't go back to renegotiate. I don't renegotiate my contracts."
 ]
 
-
-# pass in Strings of model choice and input text for context
 @st.cache
 def genQuestion(model_choice, context, tag):
-    # global descriptions
+    """Generate interview questions.
+    
+    Args:
+        model_choice: specify which BART model to use.
+        context: input text for the model.
+        tag: specify the length of the generated question (only if model_choice is 'Lengthed model').
+
+    Returns:
+        final_output: the four decoded generated sequences, separated by two newlines.
+    """
+
+    # Checks which BART model to use based on the value of model_choice
+    # Then loads a pre-trained BART model and tokenizer accordingly
     if model_choice=="Base model":
         model = BartForConditionalGeneration.from_pretrained("hyechanjun/interview-question-remake")
         tok = BartTokenizer.from_pretrained("hyechanjun/interview-question-remake")
@@ -52,8 +62,13 @@ def genQuestion(model_choice, context, tag):
         model = BartForConditionalGeneration.from_pretrained("hyechanjun/reverse-interview-question")
         tok = BartTokenizer.from_pretrained("hyechanjun/reverse-interview-question")
 
+    # Tokenize context parameter using the selected tokenizer
     inputs = tok(context, return_tensors="pt")
+
+    # Generate text using the selected pre-trained BART model
     output = model.generate(inputs["input_ids"], num_beams=4, max_length=64, min_length=9, num_return_sequences=4, diversity_penalty=1.0, num_beam_groups=4)
+
+    # Decode the generated text, then format it into a single string
     final_output = ''
     for i in range(4):
         final_output +=  [tok.decode(beam, skip_special_tokens=True, clean_up_tokenization_spaces=False) for beam in output][i] + "\n\n"
