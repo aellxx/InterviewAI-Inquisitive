1947cb1dcba5eea517c8915f14f48e7473490b92
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2023-06-22 03:21:21 -0400

    Add generate_questions function
    Provides base model question generation functionality from Senior Project 2021's Interview AI. To be improved upon.

--- question-generation/backend.py
+++ question-generation/backend.py
@@ -1,1 +1,43 @@
 #!/usr/bin/env python3
+
+from transformers import BartTokenizer, BartForConditionalGeneration
+
+
+def generate_questions(sequence: str) -> "list[str]":
+    """Generate questions based on a given sequence of context.
+
+    Args:
+        sequence: A string representing the context from which to generate questions.
+
+    Returns:
+        A list of generated questions in string format. The number of questions returned
+        is determined by the num_return_sequences parameter of the model's generate method.
+    """
+
+    model_id = "hyechanjun/interview-question-remake"
+    tokenizer = BartTokenizer.from_pretrained(model_id)
+    model = BartForConditionalGeneration.from_pretrained(model_id)
+
+    inputs = tokenizer(sequence, return_tensors="pt")
+
+    generated_ids = model.generate(
+        inputs["input_ids"],
+        num_beams=4,
+        max_length=64,
+        min_length=9,
+        num_return_sequences=4,
+        diversity_penalty=1.0,
+        num_beam_groups=4,
+    )
+
+    # Decode the generated token IDs and return the result as a list of strings
+    return tokenizer.batch_decode(
+        generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False
+    )
+
+
+if __name__ == "__main__":
+    with open("context.txt", "r") as context_txt:
+        contexts = context_txt.readlines()
+
+    print(len(generate_questions(contexts[0])))
