8c885b3646fc8bda9e218574a7e5e5568cef98d8
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2023-06-22 06:05:42 -0400

    Add load_contexts_from function
    Provides functionality to load context sequences from a text file containing them.

--- question-generation/backend.py
+++ question-generation/backend.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 
 from transformers import BartTokenizer, BartForConditionalGeneration
+from pathlib import Path
 import torch
+
+
+def load_contexts_from(file_name: Path) -> "list[str]":
+    """Retrieve context sequences from a text file.
+
+    Args:
+        file_name: A path to the text file containing the context sequences.
+          Each line represents a separate sequence.
+
+    Returns:
+        A list of context sequences as strings.
+
+    Raises:
+        FileNotFoundError: file_name does not exist.
+    """
+
+    if file_name.is_file():
+        with open(file_name, "r") as f:
+            return f.readlines()
+    else:
+        raise FileNotFoundError(f"'{file_name.name}' does not exist.")
 
 
 def generate_questions(sequence: str) -> "list[str]":
@@ -11,7 +33,7 @@ def generate_questions(sequence: str) -> "list[str]":
         sequence: A string representing the context from which to generate questions.
 
     Returns:
-        A list of generated questions in string format. The number of questions returned
+        A list of generated questions as strings. The number of questions returned
         is determined by the num_return_sequences parameter of the model's generate method.
     """
 
@@ -43,7 +65,5 @@ def generate_questions(sequence: str) -> "list[str]":
 
 
 if __name__ == "__main__":
-    with open("context.txt", "r") as context_txt:
-        contexts = context_txt.readlines()
-
-    print(generate_questions(contexts[0])[0])
+    c = load_contexts_from(Path("context.txt"))
+    print(c)
