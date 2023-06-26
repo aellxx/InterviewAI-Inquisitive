e7a690208b8e53124e57a24238aa9bf35231da27
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2023-06-26 10:39:15 -0400

    Add CORS headers

--- question-generation/main.py
+++ question-generation/main.py
@@ -3,11 +3,25 @@
 import sys
 import uvicorn
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend import get_contexts_and_questions
 
 app = FastAPI()
+
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get("/generate")
