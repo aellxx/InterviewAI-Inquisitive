d5d5c007bdd75d1dc65da249a5afe0e6e4ed9750
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2023-07-03 19:12:08 +0900

    Put eveything under main function

--- question-generation/comments-wrangling/src/taskpane/taskpane.js
+++ question-generation/comments-wrangling/src/taskpane/taskpane.js
@@ -4,39 +4,41 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Word) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = () => tryCatch(getComments);
+    document.getElementById("run").onclick = () => tryCatch(main);
   }
 });
 
-async function getComments() {
-  await Word.run(async (context) => {
-    const comments = context.document.body.getComments();
-    comments.load("items");
-    await context.sync();
-    return comments.items;
-  });
-}
-
-async function getReplies(comment) {
-  await Word.run(async (context) => {
-    const replies = comment.replies;
-    replies.load("items");
-    await context.sync();
-    return replies.items;
-  });
-}
-
-async function getContext(comment) {
-  await Word.run(async (context) => {
-    const commentContext = comment.getRange();
-    commentContext.load("text");
-    await context.sync();
-    return commentContext.text;
-  });
-}
-
 async function main() {
-  const comments = getComments();
+  await Word.run(async (context) => {
+    const comments = [];
+
+    const commentCollection = context.document.body.getComments();
+    commentCollection.load("items");
+    await context.sync();
+
+    for (let i = 0; i < commentCollection.items.length; i++) {
+      let comment = Array(commentCollection.items[i].content);
+
+      const commentReplyCollection = commentCollection.items[i].replies;
+      commentReplyCollection.load("items");
+      await context.sync();
+
+      for (let j = 0; j < commentReplyCollection.items.length; j++) {
+        comment.push(commentReplyCollection.items[j].content);
+      }
+
+      const commentRange = commentCollection.items[i].getRange();
+      commentRange.load("text");
+      await context.sync();
+
+      comments.push({
+        context: commentRange.text,
+        comments: comment,
+      });
+    }
+
+    console.log(comments);
+  });
 }
 
 export async function tryCatch(callback) {
